3b2672281dbfd2159a8d901ef357c264189d60ef
Libbie Christie <lchristi@oa-o1.msk.21-school.ru>
2022-02-23 19:26:39 +0300

    ok

--- bubble_sorting.c
+++ bubble_sorting.c
@@ -37,39 +37,75 @@
 //	return 0;
 //}
 
+//int main()
+//{
+//	int N;
+//	printf("Input N: ");
+//	scanf("%d", &N);
+//	int* mass;
+//	mass = (int *)malloc(N * sizeof(int));
+//	printf("Input the array elements:\n");
+//	for (int i = 0; i < N; i++)
+//		scanf("%d", &mass[i]);
+//	int tmp;
+//	bool noswap;
+//
+//	for (int i = N - 1; i >= 0; i--)
+//	{
+//		noswap = 1;
+//		for (int j = 0; j < i; j++)
+//		{
+//			if (mass[j] > mass[j + 1])
+//			{
+//				tmp = mass[j];
+//				mass[j] = mass[j + 1];
+//				mass[j + 1] = tmp;
+//				noswap = 0;
+//			}
+//		}
+//		if (noswap == 1)
+//			break;
+//	}
+//	printf("Sorted array:\n");
+//	for (int i = 0; i < N; i++)
+//		printf("%d ", mass[i]);
+//	printf("\n");
+//	free(mass);
+//	return 0;
+//}
+
 int main()
 {
-	int N;
-	printf("Input N: ");
-	scanf("%d", &N);
-	int* mass;
-	mass = (int *)malloc(N * sizeof(int));
-	printf("Input the array elements:\n");
-	for (int i = 0; i < N; i++)
-		scanf("%d", &mass[i]);
-	int tmp;
-	bool noswap;
+	char *array;
+	int a, b;
+	char tmp;
+	int n;
 
-	for (int i = N - 1; i >= 0; i--)
+	array = (int *)malloc(n * sizeof(int));
+	printf("input n = ");
+	scanf("%d", &n);
+	printf("Input the array elements:\n");
+	for (int i = 0; i < n; i++)
+		scanf("%d", &array[i]);
+
+
+	a = 1;
+	while(a < n)
 	{
-		noswap = 1;
-		for (int j = 0; j < i; j++)
+		++a;
+		b = n - 1;
+		while (b >= a)
 		{
-			if (mass[j] > mass[j + 1])
+			--b;
+			if (array[b - 1] > array[b])
 			{
-				tmp = mass[j];
-				mass[j] = mass[j + 1];
-				mass[j + 1] = tmp;
-				noswap = 0;
+				tmp = array[b - 1];
+				array[b - 1] = array[b];
+				array[b] = tmp;
+				printf("** %d", array[b]);
 			}
 		}
-		if (noswap == 1)
-			break;
 	}
-	printf("Sorted array:\n");
-	for (int i = 0; i < N; i++)
-		printf("%d ", mass[i]);
-	printf("\n");
-	free(mass);
-	return 0;
+
+	return(0);
 }
