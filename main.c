8e9686fdb89a762dc666851261f8c50b6b5917e0
aposipov <a.p.osipov@ro.ru>
2022-05-06 22:53:44 +1000

    insertion

--- main.c
+++ main.c
@@ -1,7 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main()
+int main(int argc, char **argv)
 {
-	printf("Hello, World!\n");
+	int *arr;
+	int i = 0;
+	int n;
+
+	if (!argv[1])
+		printf("NO argv\n");
+	printf("input number of numbers: ");
+	scanf("%d", &n);
+	printf("n = %d\n", n);
+	arr = (int *)malloc(sizeof(int) * n);
+	while (i < n)
+	{
+		scanf("%d", &arr[i]);
+		i++;
+	}
+	i = 0;
+	while (arr[i])
+	{
+		printf("num: %d\n", arr[i]);
+		i++;
+	}
 	return 0;
 }
